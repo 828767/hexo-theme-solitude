c75f5ca132f2acaee2f15fe1d5a61173df2e11a7
nobody <>
2024-05-11 15:11:10 +0800

    perf: sync fold from anzhiyu

--- scripts/tags/fold.js
+++ scripts/tags/fold.js
@@ -1,13 +1,29 @@
-'use strict'
+'use strict';
 
-function postTabs(args, content) {
-    const title = args[0] ? args[0] : ""
-    const open = args[1] ? args[1] : ""
+function postFolding(args, content) {
+  args = args.join(' ').split(',');
+  let style = ''
+  let title = ''
+  if (args.length > 1) {
+    style = args[0].trim()
+    title = args[1].trim()
+  } else if (args.length > 0) {
+    title = args[0].trim()
+  }
+  if (style != undefined) {
+    return `<details class="folding-tag" ${style}><summary> ${title} </summary>
+              <div class='content'>
+              ${hexo.render.renderSync({text: content, engine: 'markdown'}).split('\n').join('')}
+              </div>
+            </details>`;
+  } else {
+    return `<details class="folding-tag"><summary> ${title} </summary>
+              <div class='content'>
+              ${hexo.render.renderSync({text: content, engine: 'markdown'}).split('\n').join('')}
+              </div>
+            </details>`;
+  }
 
-    return `<details ${open}><summary>${title}</summary><div class="content">${hexo.render.renderSync({
-        text: content,
-        engine: 'markdown'
-    })}</div></details>`
 }
 
-hexo.extend.tag.register('fold', postTabs, {ends: true})
+hexo.extend.tag.register('folding', postFolding, {ends: true});
